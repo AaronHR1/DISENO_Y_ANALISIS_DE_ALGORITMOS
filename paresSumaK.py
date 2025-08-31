0da9998945bc402995596184ee67acc5f2bcba4a
AaronHR1 <145415107+AaronHR1@users.noreply.github.com>
2025-08-30 21:12:26 -0600

    Tarea1_ParesSumadosIgualAK
    Tarea 1.- Programa Pares de una arreglo que sumados son igual a K

---
+++ paresSumaK.py
@@ -0,0 +1,38 @@
+def solucionArreglos(arreglo,k):
+    soluciones=0
+
+    for i in range(len(arreglo)):
+        
+        for j in range(i,len(arreglo)):
+
+            if arreglo[i]+arreglo[j]==k:
+                print("solucion encontrada en el par:")
+                print(f"{arreglo[i]} en la posición: {i}")
+                print(f"{arreglo[j]} en la posición: {j}")
+                soluciones+=1
+
+    if soluciones==0:
+        print("Solucion no encontrada")
+    
+    return soluciones 
+
+def solucionHash(arreglo, k):
+    diccionario = {}
+    soluciones=0
+
+    for i in range(len(arreglo)):
+        complemento=k-arreglo[i]
+        if complemento in diccionario:
+            print("solucion encontrada en el par:")
+            print(f"{arreglo[i]} en la posición: {i}")
+            print(f"{complemento} en la posición: {diccionario[complemento]}")
+            soluciones+=1
+      
+        else:
+            diccionario[arreglo[i]]=i
+    
+    if soluciones==0:
+        print("Solucion no encontrada")
+    
+    return soluciones 
+
